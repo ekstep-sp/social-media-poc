635471b5e8b2678d285bafb8dc5cc7a57d2d4611
Rishabh Kalra <Rishabh.Kalra@in.niit.com>
2019-12-02 16:58:25 +0530

    minor changes

--- routes/twilio-whatsapp-routes/routes-main.js
+++ routes/twilio-whatsapp-routes/routes-main.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const twilioClient = require('./../../lib/twilio/twilio').getClient;
+const twilioClient = require('./../../lib/twilio/twilio');
 
 router.get('/status', (req,res) => {
     res.status(200).send('twilio apis for whatsapp are reachable');
@@ -9,7 +9,7 @@ router.get('/status', (req,res) => {
 // post api t
 router.post('/event-webhook', (req,res) => {
     console.log('recieved post request for event-webhook');
-    twilioClient.messages.create({
+    twilioClient.getClient.messages.create({
         from: 'whatsapp:+14155238886',
         to: 'whatsapp:+919971696729',
         body: 'This is automated text'

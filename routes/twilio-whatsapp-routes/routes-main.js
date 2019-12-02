27cb7a8fe71bb3bd4c52013d5eedf6b198c9dce6
Rishabh Kalra <Rishabh.Kalra@in.niit.com>
2019-12-02 16:56:08 +0530

    minor changes

--- routes/twilio-whatsapp-routes/routes-main.js
+++ routes/twilio-whatsapp-routes/routes-main.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+const twilioClient = require('./../../lib/twilio/twilio').getClient;
+
 router.get('/status', (req,res) => {
     res.status(200).send('twilio apis for whatsapp are reachable');
 })
@@ -7,13 +9,21 @@ router.get('/status', (req,res) => {
 // post api t
 router.post('/event-webhook', (req,res) => {
     console.log('recieved post request for event-webhook');
-    const completeData = {
+    twilioClient.messages.create({
+        from: 'whatsapp:+14155238886',
+        to: 'whatsapp:+919971696729',
+        body: 'This is automated text'
+    }).then(messageRes => {
+        console.log('message sent with id ', messageRes.sid);
+        res.send('OK');
+    });
+    /* const completeData = {
         body: req.body,
         params: req.params,
         headers: req.headers
     }
     console.log('complete req is ', completeData);
-    res.status(200).send({completeData});
+    res.status(200).send({completeData}); */
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,12 +8,6 @@ const mainPort = process.env.PORT;
 // importing routes from different routers
 const {twRouter} = require('./routes/routes-main');
 // importing twilio for whatsapp
-const twilio = require('twilio');
-
-// importing account sid and account token of whatsapp application installed in twilio
-const accountSid = 'AC2dc051e81b708fa2e98a76bd1bfcb6e6'
-const authToken = '5242098a97503afe35c3e2faa04f2e68'
-// const client = twilio(accountSid, authToken);
 
 app.use(cors());
 app.use(bodyParser.json());

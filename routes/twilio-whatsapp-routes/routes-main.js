78ce2aabddb7534703bcf366c20af117f456d7f5
Rishabh Kalra <Rishabh.Kalra@in.niit.com>
2019-12-02 13:00:28 +0530

    webhook url for whatsapp api

--- routes/twilio-whatsapp-routes/routes-main.js
+++ routes/twilio-whatsapp-routes/routes-main.js
@@ -4,4 +4,11 @@ router.get('/status', (req,res) => {
     res.status(200).send('twilio apis for whatsapp are reachable');
 })
 
+// post api t
+router.post('/event-webhook', (req,res) => {
+    console.log('recieved post request for status');
+    console.log(req.body);
+    res.status(200).send({response: 'ok'});
+});
+
 module.exports = router;

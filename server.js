d93e8b6cb542a0159a25d2a89e4f1dd90cfe3151
Rishabh Kalra <Rishabh.Kalra@in.niit.com>
2019-12-03 10:35:27 +0530

    minor changes

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const mainPort = process.env.PORT;
 // importing routes from different routers
 const {twRouter} = require('./routes/routes-main');
 // importing twilio for whatsapp
-
-app.use(cors());
-app.use(bodyParser.json());
+app.use(express.urlencoded())
+// app.use(cors());
+// app.use(bodyParser.json());
 app.get('/status', (req,res) => {
     res.status(200).send('I am alive');
 })

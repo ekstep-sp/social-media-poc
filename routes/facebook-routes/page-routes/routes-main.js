f6ea226e776130606178acb42190c8eb0189ed10
Rishabh Kalra <Rishabh.Kalra@in.niit.com>
2019-12-09 12:07:57 +0530

    converted fb-callback url to accept all methods

--- routes/facebook-routes/page-routes/routes-main.js
+++ routes/facebook-routes/page-routes/routes-main.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
 
+const appDetails = {
+    appName: 'social-media-usage',
+    appID: '554877028637015',
+    userToken: 'EAAH4qGAvmVcBADZCt2BOMjRR6OZBVO8mXgoi3rJ5253lffoZAaEab9HJvCDLHCyvg8lopOuiKVQt2CQNaj1ZB4aTR8HdkrD70MQtkTdsEYHMjpwBIZB8XzagSreUJAXYW2SrSZCbRCOeOSlh83ITglWPbdQhD0ymdxb2PLyOTQMAZDZD',
+    appToken: '554877028637015|JD6fnGr9fiLO3Qc9Ed8KXwO81-w'
+}
+
+
 
 
 router.get('/health', (req,res) => {
@@ -7,10 +15,11 @@ router.get('/health', (req,res) => {
     res.status(200).send({health: 'ok'});
 });
 
-router.post('/page-event', (req, res) => {
-    console.log('\nfb-callback/page-event hit\n');
+router.all('/page-event', (req, res, next) => {
+    console.log(`\nfb-callback/page-event hit --> ${req.method}\n`);
     console.log('recieved data is ', req.body);
     res.status(200).send({status: 'ok', message: 'webhook recieved properly'});
+    next();
 });
 
 module.exports = router;
